11550756b65bbcacc1db1a704e21ad615aa6e97a
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-08-24 20:48:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup() {
+  canvas = createCanvas(400, 300);
+  canvas.position(500,200);
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function preload(){
+
+}
+
+function draw(){
+  
+}
+
+
+
